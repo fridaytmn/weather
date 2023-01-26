c95132b4bb88e5a927781028f1f3e8124345516b
Vlad Shvets <88648536+fridaytmn@users.noreply.github.com>
2023-01-26 22:36:43 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USE_ROUNDED_COORDS = True
-OPENWEATHER_API = "b038d94bc7127f9856ee39f0e6dbe2b9"
+OPENWEATHER_API = "<api_token_open_weather>"
 OPENWEATHER_URL = ("https://api.openweathermap.org/data/2.5/weather?"+
                    "lat={latitude}&lon={longitude}&"+
                    "appid="+OPENWEATHER_API)
